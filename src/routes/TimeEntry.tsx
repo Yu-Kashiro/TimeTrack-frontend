67327070babd0ed615533ece0810ede5a35c409c
Yu-Kashiro <poyonnnnnn@gmail.com>
2025-05-31 16:49:25 +0900

    jsxをtsxに修正

---
+++ src/routes/TimeEntry.tsx
@@ -0,0 +1,8 @@
+export const TimeEntry = () => {
+  return (
+    <div className="flex flex-col gap-4">
+      <h1 className="text-2xl font-bold">Time Entry</h1>
+      <p className="text-gray-600">This is the time entry page.</p>
+    </div>
+  );
+}
