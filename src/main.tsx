d693295aa2941e0da9731c10c290f4ba66bde956
Yu-Kashiro <poyonnnnnn@gmail.com>
2025-05-31 16:54:02 +0900

    routeを設定

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,20 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { Provider } from './components/ui/provider.tsx'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { TimeEntry } from './routes/TimeEntry.tsx'
+import { SignUp } from './routes/SignUp.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider>
-      <App />
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<App />} />
+          <Route path="time-entry" element={<TimeEntry />} />
+          <Route path="signup" element={<SignUp />} />
+        </Routes>
+      </BrowserRouter>
     </Provider>
   </StrictMode>,
 )
