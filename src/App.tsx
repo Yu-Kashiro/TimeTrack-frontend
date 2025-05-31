f43fa0303943f733f408b9724dcd3763f58ab724
Yu-Kashiro <poyonnnnnn@gmail.com>
2025-05-31 22:50:40 +0900

    ホーム画面を編集

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,19 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Link } from 'react-router-dom'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <div>
+        <Link to={"/time-entry"}>時間入力画面</Link>
+      </div>
+      <div>
+        <Link to={"/signup"}>サインアップ画面</Link>
+      </div>
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
