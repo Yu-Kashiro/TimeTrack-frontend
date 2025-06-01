e081267eb2a263c07d9c9849e5d97f05d04c53e0
Yu-Kashiro <poyonnnnnn@gmail.com>
2025-06-01 10:05:47 +0900

    Railsロゴを追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import railsLogo from './assets/rails.svg'
 import './App.css'
 import { Link } from 'react-router-dom'
+import { HStack } from '@chakra-ui/react'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
@@ -15,26 +15,18 @@ function App() {
       <div>
         <Link to={"/signup"}>サインアップ画面</Link>
       </div>
-      <div>
+      <HStack justify="center">
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        <a href="https://rubyonrails.org" target="_blank">
+          <img src={railsLogo} className="logo rails" alt="Rails logo" />
+        </a>
+      </HStack>
+      <h5>Powered by Vite + React + Rails</h5>
     </>
   )
 }
