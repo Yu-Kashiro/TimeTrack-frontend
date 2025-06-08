fa84b42e7bcbb29d34e3c51cc26e0b58abecee17
Yu-Kashiro <poyonnnnnn@gmail.com>
2025-06-08 22:39:39 +0900

    出勤登録ページを編集

--- src/routes/Registration.tsx
+++ src/routes/Registration.tsx
@@ -1,76 +1,161 @@
-import { Box, Button, Field, Flex, Heading, Input, Stack } from "@chakra-ui/react";
+import { createWorkTime } from "@/lib/api/workTime";
+import {
+  Box,
+  Button,
+  Field,
+  Flex,
+  Heading,
+  Input,
+  Stack,
+  Switch,
+} from "@chakra-ui/react";
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export const Registration = () => {
   const todayDate = new Date().toISOString().split("T")[0];
+  const [workDate, setWorkDate] = useState(todayDate);
+  const [clockIn, setClockIn] = useState("08:30");
+  const [clockOut, setClockOut] = useState("17:15");
   const [breakTimeMinute, setBreakTimeMinute] = useState("01:00");
-  console.log(todayDate);
+  const [memo, setMemo] = useState("");
+  const [isPaidVacation, setIsPaidVacation] = useState(false);
+  const navigate = useNavigate();
+
+  const createEvent = async (event) => {
+    await createCalendarEvent({
+      title: event.title,
+      description: event.description,
+      startDate: event.startDate,
+      endDate: event.endDate,
+    });
+  };
 
   return (
     <Stack>
-      <Heading size="2xl" mb={5}>出勤登録</Heading>
+      <Heading size="2xl" mb={5}>
+        出勤登録
+      </Heading>
       <Field.Root>
         <Box width="100%">
           <Flex align="center" gap={4}>
-            <Field.Label whiteSpace="nowrap" minWidth="80px">日付</Field.Label>
-            <Input type="date" value={todayDate} width="100%" />
-          </Flex>
-        </Box>
-
-        <Box width="100%">
-          <Flex align="center" gap={4} mt={3}>
-            <Field.Label whiteSpace="nowrap" minWidth="80px">始業時間</Field.Label>
-            <Input type="time" value="08:30" />
-          </Flex>
-        </Box>
-
-        <Box width="100%">
-          <Flex align="center" gap={4} mt={3}>
-            <Field.Label whiteSpace="nowrap" minWidth="80px">終業時間</Field.Label>
-            <Input type="time" value="17:15" />
-          </Flex>
-        </Box>
-
-        <Box width="100%">
-          <Flex align="center" gap={4} mt={3}>
-            <Field.Label whiteSpace="nowrap" minWidth="80px">休憩時間</Field.Label>
+            <Field.Label whiteSpace="nowrap" minWidth="80px">
+              日付
+            </Field.Label>
             <Input
-              type="time"
-              value={breakTimeMinute}
-              step="60"
-              onChange={(e) => setBreakTimeMinute(e.target.value)}
+              type="date"
+              value={workDate}
+              width="100%"
+              onChange={(e) => setWorkDate(e.target.value)}
             />
           </Flex>
         </Box>
 
         <Box width="100%">
-          <Flex align="center" gap={4}  mt={3}>
-            <Field.Label whiteSpace="nowrap" minWidth="80px">備考</Field.Label>
-            <Input type="text" autoComplete="off" />
+          <Flex align="center" gap={4} mt={3}>
+            <Field.Label whiteSpace="nowrap" minWidth="80px">
+              始業時間
+            </Field.Label>
+            <Input
+              type="time"
+              value={clockIn}
+              onChange={(e) => setClockIn(e.target.value)}
+              disabled={isPaidVacation}
+            />
+          </Flex>
+        </Box>
+
+        <Box width="100%">
+          <Flex align="center" gap={4} mt={3}>
+            <Field.Label whiteSpace="nowrap" minWidth="80px">
+              終業時間
+            </Field.Label>
+            <Input
+              type="time"
+              value={clockOut}
+              onChange={(e) => setClockOut(e.target.value)}
+              disabled={isPaidVacation}
+            />
+          </Flex>
+        </Box>
+
+        <Box width="100%">
+          <Flex align="center" gap={4} mt={3}>
+            <Field.Label whiteSpace="nowrap" minWidth="80px">
+              休憩時間
+            </Field.Label>
+            <Input
+              type="time"
+              value={breakTimeMinute}
+              step="60"
+              onChange={(e) => setBreakTimeMinute(e.target.value)}
+              disabled={isPaidVacation}
+            />
+          </Flex>
+        </Box>
+
+        <Box width="100%">
+          <Flex align="center" gap={4} mt={3}>
+            <Field.Label whiteSpace="nowrap" minWidth="80px">
+              備考
+            </Field.Label>
+            <Input
+              type="text"
+              value={memo}
+              autoComplete="off"
+              onChange={(e) => setMemo(e.target.value)}
+            />
           </Flex>
         </Box>
       </Field.Root>
+
+      <Flex justify="flex-end" mt={3}>
+        <Switch.Root
+          colorPalette="blue"
+          checked={isPaidVacation}
+          onCheckedChange={(e) => {
+            setIsPaidVacation(e.checked);
+            if (e.checked) {
+              setClockIn("");
+              setClockOut("");
+              setBreakTimeMinute("");
+            } else {
+              setClockIn("08:30");
+              setClockOut("17:15");
+              setBreakTimeMinute("01:00");
+            }
+          }}
+        >
+          <Switch.HiddenInput />
+          <Switch.Control />
+          <Switch.Label>有給休暇を使用する</Switch.Label>
+        </Switch.Root>
+      </Flex>
+
       <Button
         colorPalette={"blue"}
         variant="subtle"
-        type="submit"
+        onClick={() => {
+          createWorkTime({
+            workDate,
+            clockIn,
+            clockOut,
+            breakTimeMinute,
+            memo,
+            isPaidVacation,
+          });
+          navigate("/work_times/index");
+        }}
         mt={5}
         color={"black"}
         size="xl"
       >
-        出勤時間を登録する
+        登録する
       </Button>
 
-      <Button
-        variant="subtle"
-        type="submit"
-        mt={5}
-        color={"black"}
-        size="xl"
-      >
+      <Button variant="subtle" type="submit" mt={5} color={"black"} size="xl">
         出勤状況を確認する
       </Button>
-
     </Stack>
   );
 };
