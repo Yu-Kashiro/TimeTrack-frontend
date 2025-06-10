dfe88a565d9880c1e69a3e5f1945807671355e6a
Yu-Kashiro <poyonnnnnn@gmail.com>
2025-06-10 23:08:49 +0900

    不要な画像を削除

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,27 +1,22 @@
-import { HStack } from "@chakra-ui/react/stack";
-import reactLogo from "../assets/react.svg";
-import viteLogo from "/vite.svg";
-import railsLogo from "../assets/rails.svg";
 import { ColorModeButton } from "./ui/color-mode";
 import { Box } from "@chakra-ui/react/box";
+import { Link } from "react-router-dom";
 
 export const Footer = () => {
   return (
     <>
+      <Box mt={50}>
+        <Link to="/work_times/detail">detail</Link>
+      </Box>
+      <Box>
+        <Link to="/work_times/index">index</Link>
+      </Box>
+      <Box>
+        <Link to="/work_times/registration">registration</Link>
+      </Box>
       <Box mt={120}>
         <ColorModeButton />
       </Box>
-      <HStack justify="center">
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-        <a href="https://rubyonrails.org" target="_blank">
-          <img src={railsLogo} className="logo rails" alt="Rails logo" />
-        </a>
-      </HStack>
     </>
   );
 };
