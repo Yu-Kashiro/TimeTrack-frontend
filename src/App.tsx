369dae696a3243c3aca2e83c633a3fdc81f26832
Yu-Kashiro <poyonnnnnn@gmail.com>
2025-06-14 23:51:10 +0900

    routeを修正

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,22 @@
 import "./App.css";
-import { Box } from "@chakra-ui/react";
-import { Footer } from "./components/Footer.js";
-import { Outlet } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
+import { SignIn } from "./pages/SignIn.js";
+import { SignUp } from "./pages/SignUp.js";
+import { Registration } from "./pages/Registration.js";
+import { WorkTimes } from "./pages/WorkTimes.js";
+import { WorkTimesDetail } from "./pages/WorkTimeDetail.js";
+import { NotFound } from "./pages/NotFound.js";
 
 export const App = () => {
   return (
-    <>
-      <Outlet />
-      <Box>
-        <Footer />
-      </Box>
-    </>
+    <Routes>
+      <Route index element={<Navigate to="/signin" replace />} />
+      <Route path="/signin" element={<SignIn />} />
+      <Route path="/signup" element={<SignUp />} />
+      <Route path="/work_times" element={<WorkTimes />} />
+      <Route path="/work_times/:workTimesId" element={<WorkTimesDetail />} />
+      <Route path="/work_times/registration" element={<Registration />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
   );
 };
--- src/main.tsx
+++ src/main.tsx
@@ -3,27 +3,13 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import { App } from './App.tsx'
 import { Provider } from './components/ui/provider.tsx'
-import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
-import { Registration } from './pages/Registration.tsx'
-import { SignUp } from './pages/SignUp.tsx'
-import { SignIn } from './pages/SignIn.tsx'
-import { WorkTimes } from './pages/WorkTimes.tsx'
-import { Detail } from './pages/WorkTimeDetail.tsx'
+import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider>
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<App />}>
-            <Route index element={<Navigate to="signin" replace />} />
-            <Route path="signin" element={<SignIn />} />
-            <Route path="signup" element={<SignUp />} />
-            <Route path="work_times/registration" element={<Registration />} />
-            <Route path="work_times/index" element={<WorkTimes />} />
-            <Route path="work_times/detail" element={<Detail />} />
-          </Route>
-        </Routes>
+        <App />
       </BrowserRouter>
     </Provider>
   </StrictMode>,
