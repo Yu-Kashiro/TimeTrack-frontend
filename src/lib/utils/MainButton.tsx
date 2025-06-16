9cfb5bcc7221f70ca5558f5e57f0e80ace96fa4f
Yu-Kashiro <poyonnnnnn@gmail.com>
2025-06-16 22:56:29 +0900

    引数を追加

--- src/lib/utils/MainButton.tsx
+++ src/lib/utils/MainButton.tsx
@@ -4,17 +4,20 @@ import type { ReactNode } from "react";
 type MainButtonProps = {
   colorPalette: string;
   color: string;
+  onClick: () => void;
   children: ReactNode
 }
 
-export const MainButton = ({colorPalette, color, children }: MainButtonProps) => {
+export const MainButton = ({colorPalette, color, onClick,  children }: MainButtonProps) => {
   return (
     <Button
       colorPalette={colorPalette}
       variant="subtle"
+      size="xl"
       type="submit"
       mt={5}
       color={color}
+      onClick={onClick}
     >
       {children}
     </Button>
