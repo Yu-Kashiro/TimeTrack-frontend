939240f3066ce831b178eac0de46e9756807670c
Yu-Kashiro <poyonnnnnn@gmail.com>
2025-06-16 22:55:39 +0900

    ルーティングを変更

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,8 @@ import { Navigate, Route, Routes } from "react-router-dom";
 import { SignIn } from "./pages/SignIn.js";
 import { SignUp } from "./pages/SignUp.js";
 import { Registration } from "./pages/Registration.js";
-import { WorkTimes } from "./pages/WorkTimes.js";
-import { WorkTimesDetail } from "./pages/WorkTimeDetail.js";
+import { WorkTimesTable } from "./pages/WorkTimesTable.js";
+import { WorkTimesDetail } from "./pages/WorkTimesDetail.js";
 import { NotFound } from "./pages/NotFound.js";
 
 export const App = () => {
@@ -13,9 +13,10 @@ export const App = () => {
       <Route index element={<Navigate to="/signin" replace />} />
       <Route path="/signin" element={<SignIn />} />
       <Route path="/signup" element={<SignUp />} />
-      <Route path="/work_times" element={<WorkTimes />} />
+      <Route path="/work_times" element={<WorkTimesTable />} />
       <Route path="/work_times/:workTimesId" element={<WorkTimesDetail />} />
       <Route path="/work_times/registration" element={<Registration />} />
+      <Route path="/work_times/registration/:workTimesId" element={<Registration />} />
       <Route path="*" element={<NotFound />} />
     </Routes>
   );
