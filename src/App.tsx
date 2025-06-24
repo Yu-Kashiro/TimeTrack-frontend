5eaeb2662bca4f09f97629e6a5b734658aa4f862
Yu-Kashiro <poyonnnnnn@gmail.com>
2025-06-24 16:52:48 +0900

    RESTfulなルート名に変更

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,10 @@ import { Navigate, Route, Routes } from "react-router-dom";
 import { SignIn } from "./pages/SignIn.js";
 import { SignUp } from "./pages/SignUp.js";
 import { Registration } from "./pages/Registration.js";
-import { WorkTimesTable } from "./pages/WorkTimesTable.js";
-import { WorkTimesDetail } from "./pages/WorkTimesDetail.js";
+import { WorkTimes } from "./pages/WorkTimes.js";
+import { WorkTimeDetail } from "./pages/WorkTimeDetail.js";
 import { NotFound } from "./pages/NotFound.js";
-import { UpdateWorkTimes } from "./pages/UpdateWorkTimes.js";
+import { UpdateWorkTime } from "./pages/UpdateWorkTime.js";
 
 export const App = () => {
   return (
@@ -14,10 +14,13 @@ export const App = () => {
       <Route index element={<Navigate to="/signin" replace />} />
       <Route path="/signin" element={<SignIn />} />
       <Route path="/signup" element={<SignUp />} />
-      <Route path="/work_times" element={<WorkTimesTable />} />
-      <Route path="/work_times/:workTimesId" element={<WorkTimesDetail />} />
+      <Route path="/work_times" element={<WorkTimes />} />
+      <Route path="/work_times/:workTimeId" element={<WorkTimeDetail />} />
       <Route path="/work_times/registration" element={<Registration />} />
-      <Route path="/work_times/registration/:workTimesId" element={<UpdateWorkTimes />} />
+      <Route
+        path="/work_times/registration/:workTimeId"
+        element={<UpdateWorkTime />}
+      />
       <Route path="*" element={<NotFound />} />
     </Routes>
   );
