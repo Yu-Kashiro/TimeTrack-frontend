10dc01026cc0378c0a98634a71fd78eac245b8d6
Yu-Kashiro <poyonnnnnn@gmail.com>
2025-06-26 15:35:39 +0900

    colorを修正

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,15 @@ import "./index.css";
 import { App } from "./App.tsx";
 import { Provider } from "./components/ui/provider.tsx";
 import { BrowserRouter } from "react-router-dom";
+import { LightMode } from "./components/ui/color-mode.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Provider>
       <BrowserRouter>
-        <App />
+        <LightMode>
+          <App />
+        </LightMode>
       </BrowserRouter>
     </Provider>
   </StrictMode>
