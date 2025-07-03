86964142f2415a8bfe68828ab01754c13ef6a881
Yu-Kashiro <poyonnnnnn@gmail.com>
2025-07-03 21:54:11 +0900

    404ページのデザインを修正

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,10 +1,12 @@
 import { Box } from "@chakra-ui/react";
+import { Layout } from "@/lib/components/Layout";
 
 export const NotFound = () => {
   return (
-    <>
-      <Box>404エラー</Box>
-      <Box>ページが見つかりませんでした。</Box>
-    </>
+    <Layout title="404エラー">
+      <Box textAlign="center">
+        ページが見つかりませんでした。
+      </Box>
+    </Layout>
   );
 };
