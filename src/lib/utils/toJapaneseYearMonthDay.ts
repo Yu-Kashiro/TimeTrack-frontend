20a59ea8fde74a4997a1f2cb3dce7e494580f17d
Yu-Kashiro <poyonnnnnn@gmail.com>
2025-07-05 23:36:21 +0900

    出勤詳細ページをリファクタリング

---
+++ src/lib/utils/toJapaneseYearMonthDay.ts
@@ -0,0 +1,4 @@
+export const toJapaneseYearMonthDay = (dateString: string) => {
+    const date = new Date(dateString);
+    return `${date.getFullYear()}年${date.getMonth() + 1}月${date.getDate()}日`;
+  };
