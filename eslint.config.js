26181111a7887d3620963b7bbb35c542bd0f3b3b
Yu-Kashiro <poyonnnnnn@gmail.com>
2025-07-07 22:17:55 +0900

    ESLintとPrettierの設定を調整

--- eslint.config.js
+++ eslint.config.js
@@ -4,36 +4,40 @@ import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 import tseslint from "typescript-eslint";
 import eslintReact from "@eslint-react/eslint-plugin";
+import eslintConfigPrettier from "eslint-config-prettier/flat";
 
-export default tseslint.config(
-  { ignores: ["dist"] },
-  {
-    extends: [
-      js.configs.recommended,
-      ...tseslint.configs.recommended,
-      eslintReact.configs["recommended-typescript"],
-    ],
-    files: ["**/*.{ts,tsx}"],
-    languageOptions: {
-      parser: tseslint.parser,
-      parserOptions: {
-        projectService: true,
-        tsconfigRootDir: import.meta.dirname,
-      },
-      ecmaVersion: 2020,
-      globals: globals.browser,
-    },
-    plugins: {
-      "react-hooks": reactHooks,
-      "react-refresh": reactRefresh,
-    },
-    rules: {
-      ...reactHooks.configs.recommended.rules,
-      "react-refresh/only-export-components": [
-        "warn",
-        { allowConstantExport: true },
+export default [
+  ...tseslint.config(
+    { ignores: ["dist"] },
+    {
+      extends: [
+        js.configs.recommended,
+        ...tseslint.configs.recommended,
+        eslintReact.configs["recommended-typescript"],
       ],
-      "@eslint-react/no-missing-key": "warn",
-    },
-  }
-);
+      files: ["**/*.{ts,tsx}"],
+      languageOptions: {
+        parser: tseslint.parser,
+        parserOptions: {
+          projectService: true,
+          tsconfigRootDir: import.meta.dirname,
+        },
+        ecmaVersion: 2020,
+        globals: globals.browser,
+      },
+      plugins: {
+        "react-hooks": reactHooks,
+        "react-refresh": reactRefresh,
+      },
+      rules: {
+        ...reactHooks.configs.recommended.rules,
+        "react-refresh/only-export-components": [
+          "warn",
+          { allowConstantExport: true },
+        ],
+        "@eslint-react/no-missing-key": "warn",
+      },
+    }
+  ),
+  eslintConfigPrettier,
+];
