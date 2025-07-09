ec04b6d509eecf6404c3b3cdeacc2b615705e199
Yu-Kashiro <poyonnnnnn@gmail.com>
2025-07-09 10:26:00 +0900

    Delete src/lib/components/breakMinuteOptions.tsx

--- src/lib/components/breakMinuteOptions.tsx
+++
@@ -1,9 +0,0 @@
-export const BreakMinuteOptions = () => (
-  <>
-    <option value="00">0分</option>
-    <option value="15">15分</option>
-    <option value="30">30分</option>
-    <option value="45">45分</option>
-
-  </>
-);
