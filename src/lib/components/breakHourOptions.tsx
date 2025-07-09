e70b877efb3b97758b98ca6b1767ed988a6a9d4d
Yu-Kashiro <poyonnnnnn@gmail.com>
2025-07-09 10:26:12 +0900

    Delete src/lib/components/breakHourOptions.tsx

--- src/lib/components/breakHourOptions.tsx
+++
@@ -1,9 +0,0 @@
-export const BreakHourOptions = () => (
-  <>
-    <option value="00">0時間</option>
-    <option value="01">1時間</option>
-    <option value="02">2時間</option>
-    <option value="03">3時間</option>
-
-  </>
-);
