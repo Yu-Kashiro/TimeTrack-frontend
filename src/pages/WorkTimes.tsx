13c4e4f5179d27c70bd7dbb18cc05307bb71bd81
Yu-Kashiro <poyonnnnnn@gmail.com>
2025-07-11 10:38:56 +0900

    refactor: セレクトボックスをコンポーネント化

--- src/pages/WorkTimes.tsx
+++ src/pages/WorkTimes.tsx
@@ -1,5 +1,5 @@
 import { getWorkTimesAll } from "@/lib/api/workTimes";
-import { createListCollection, Portal, Select, Table } from "@chakra-ui/react";
+import { createListCollection, Table } from "@chakra-ui/react";
 import { useNavigate } from "react-router-dom";
 import { useState, useEffect } from "react";
 import { Layout } from "@/lib/components/Layout";
@@ -14,6 +14,7 @@ import { useLogout } from "@/lib/hooks/useLogout";
 import { WorkTimesFooter } from "@/lib/components/WorkTimesFooter";
 import { WorkTimesHeader } from "@/lib/components/WorkTimesHeader";
 import { getCurrentYearMonth } from "@/lib/utils/getCurrentYearMonth";
+import { SelectBox } from "@/lib/components/SelectBox";
 
 export const WorkTimes = () => {
   const navigate = useNavigate();
@@ -62,7 +63,7 @@ export const WorkTimes = () => {
     yearAndMonthsSet.add(yearAndMonthStr);
   });
 
-  // ChakraUIテーブル用のCollectionフォーマットを作成
+  // 勤務月の選択肢リストを作成
   const uniqueMonthListCollection = createListCollection({
     items: Array.from(yearAndMonthsSet)
       .sort()
@@ -89,49 +90,12 @@ export const WorkTimes = () => {
 
   return (
     <Layout title="勤務一覧">
-      <Select.Root
-        collection={uniqueMonthListCollection}
-        value={[selectedMonth]}
-        onValueChange={(e) => setSelectedMonth(e.value[0])}
-        size="md"
-        defaultValue={[selectedMonth]} //初期値は現在の年月
-      >
-        <Select.HiddenSelect />
-        <Select.Control>
-          <Select.Trigger>
-            <Select.ValueText placeholder="年月を選択してください" />
-          </Select.Trigger>
-          <Select.IndicatorGroup>
-            <Select.Indicator />
-          </Select.IndicatorGroup>
-        </Select.Control>
-        <Portal>
-          <Select.Positioner>
-            <Select.Content
-              bg="white"
-              borderColor="gray.200"
-              boxShadow="md"
-              className="chakra-theme light"
-            >
-              {uniqueMonthListCollection.items.map((uniqueMonth) => (
-                <Select.Item
-                  item={uniqueMonth}
-                  key={uniqueMonth.value}
-                  bg="white"
-                  color="black"
-                  _hover={{ bg: "gray.50" }}
-                  _selected={{ bg: "blue.50", color: "blue.600" }}
-                >
-                  {uniqueMonth.label}
-                  <Select.ItemIndicator />
-                </Select.Item>
-              ))}
-            </Select.Content>
-          </Select.Positioner>
-        </Portal>
-      </Select.Root>
+      <SelectBox
+        uniqueMonthListCollection={uniqueMonthListCollection}
+        selectedMonth={selectedMonth}
+        setSelectedMonth={setSelectedMonth}
+      />
 
-      {/* テーブル */}
       <Table.Root size="sm" variant="outline">
         <WorkTimesHeader />
 
